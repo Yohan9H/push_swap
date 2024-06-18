e5fef6e1f76d16a00449089611d1a61b998cea94
Yohan Hurtebize <yohurteb@made-f0Br5s3.clusters.42paris.fr>
2024-06-18 11:10:45 +0200

    add

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:04:51 by yohurteb          #+#    #+#             */
-/*   Updated: 2024/06/18 10:01:53 by yohurteb         ###   ########.fr       */
+/*   Updated: 2024/06/18 11:09:09 by yohurteb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,36 @@ static void	simple_or_radix(t_list **stack_a, t_list **stack_b)
 		radix_sort(stack_a, stack_b);
 }
 
+static void	clean_split(char **argv, int argc)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 1)
+	{
+		while (argv[i])
+		{
+			free(argv[i]);
+			i++;
+		}
+		free(argv);
+	}
+}
+
+static void	main_too_long(t_list *stack_a, t_list *stack_b, char **argv, int argc)
+{
+	if (is_sorted(&stack_a) == 0)
+	{
+		ft_lstclear(&stack_a);
+		ft_lstclear(&stack_b);
+		return ;
+	}
+	simple_or_radix(&stack_a, &stack_b);
+	clean_split(argv, argc);
+	ft_lstclear(&stack_a);
+	ft_lstclear(&stack_b);
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
@@ -27,7 +57,11 @@ int	main(int argc, char **argv)
 
 	argc--;
 	if (argc == 1)
+	{
 		argv = ft_split(argv[1], ' ');
+		if (!argv)
+			return (1);
+	}
 	if (check_args(argc, argv) == 1)
 	{
 		write(2, "Error\n", 6);
@@ -36,14 +70,6 @@ int	main(int argc, char **argv)
 	stack_a = NULL;
 	stack_a = parser(argv, &stack_a);
 	stack_b = NULL;
-	if (is_sorted(&stack_a) == 0)
-	{
-		ft_lstclear(&stack_a);
-		ft_lstclear(&stack_b);
-		return (0);
-	}
-	simple_or_radix(&stack_a, &stack_b);
-	ft_lstclear(&stack_a);
-	ft_lstclear(&stack_b);
+	main_too_long(stack_a, stack_b, argv, argc);
 	return (0);
 }
